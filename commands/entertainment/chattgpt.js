aceb74207cbdea13f7db5a59c9149b15f9e9f407
refisetiawan75 <44205531+refisetiawan75@users.noreply.github.com>
2023-06-12 00:15:02 +0700

    chattgpt.js

--- commands/entertainment/chattgpt.js
+++ commands/entertainment/chattgpt.js
@@ -8,7 +8,7 @@ module.exports = {
     type: "entertainment",
     example: `%prefix%command`,
     start: async(killua, m, { text }) => {
-        let fetch = await fetchUrl(global.api("zenz", "/entertainment/chattgpt", { query: text }, "apikey"))
+        let fetch = await fetchUrl(global.api("zenz", "/entertainment/chattgpt", { query: text }, "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"))
         killua.sendText(m.from, fetch.result, m)
     },
 }
